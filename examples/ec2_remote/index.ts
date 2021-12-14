2e6fc0af074d22a87f8b07db87c03fd743a56f08
Luke Hoban <luke@pulumi.com>
2021-12-14 14:08:41 -0800

    Initial GHA configuration (#13)
    Setup `master`, `pull_request` and `release` actions for this package.
    
    Fix up and improve some test coverage.
    
    Fixes #2.
    Fixes #3.
    Part of #11.

--- examples/ec2_remote/index.ts
+++ examples/ec2_remote/index.ts
@@ -1,5 +1,5 @@
 import { interpolate, Config } from "@pulumi/pulumi";
-import { local, remote } from "@pulumi/command";
+import { local, remote, types } from "@pulumi/command";
 import * as aws from "@pulumi/aws";
 import * as fs from "fs";
 import * as os from "os";
@@ -7,7 +7,6 @@ import * as path from "path";
 import { size } from "./size";
 
 const config = new Config();
-const publicKey = config.get("publicKey") ?? fs.readFileSync(path.join(os.homedir(), ".ssh", "id_rsa.pub")).toString("utf8");
 const keyName = config.get("keyName") ?? new aws.ec2.KeyPair("key", { publicKey: config.require("publicKey") }).keyName;
 const privateKeyBase64 = config.get("privateKeyBase64");
 const privateKey = privateKeyBase64 ? Buffer.from(privateKeyBase64, 'base64').toString('ascii') : fs.readFileSync(path.join(os.homedir(), ".ssh", "id_rsa")).toString("utf8");
@@ -36,7 +35,7 @@ const server = new aws.ec2.Instance("server", {
     vpcSecurityGroupIds: [secgrp.id],
 }, { replaceOnChanges: ["instanceType"] });
 
-const connection: remote.types.input.ConnectionArgs = {
+const connection: types.input.remote.ConnectionArgs = {
     host: server.publicIp,
     user: "ec2-user",
     privateKey: privateKey,
