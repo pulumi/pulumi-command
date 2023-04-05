c5553f1e24cdb9322616516f5486336b4da777e1
Marc Ende <me@e-beyond.de>
2023-04-05 07:54:55 +0200

    Added test

---
+++ examples/ec2_remote_proxy/index.ts
@@ -0,0 +1,96 @@
+import { interpolate, Config, secret } from "@pulumi/pulumi";
+import { local, remote, types } from "@pulumi/command";
+import * as aws from "@pulumi/aws";
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+import { size } from "./size";
+
+const config = new Config();
+const keyName = config.get("keyName") ?? new aws.ec2.KeyPair("key", { publicKey: config.require("publicKey") }).keyName;
+const privateKeyBase64 = config.get("privateKeyBase64");
+const privateKey = privateKeyBase64 ? Buffer.from(privateKeyBase64, 'base64').toString('ascii') : fs.readFileSync(path.join(os.homedir(), ".ssh", "id_rsa")).toString("utf8");
+
+const secgrp = new aws.ec2.SecurityGroup("secgrp", {
+    description: "Foo",
+    ingress: [
+        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+        { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] },
+    ],
+    egress: [
+        { fromPort: 0, toPort: 0, protocol: "-1", cidrBlocks: ["0.0.0.0/0"], ipv6CidrBlocks: ["::/0"], },
+    ],
+});
+
+const ami = aws.ec2.getAmiOutput({
+    owners: ["amazon"],
+    mostRecent: true,
+    filters: [{
+        name: "name",
+        values: ["amzn-ami-hvm-*-x86_64-gp2"],
+    }],
+});
+
+const server = new aws.ec2.Instance("server", {
+    instanceType: size,
+    ami: ami.id,
+    keyName: keyName,
+    vpcSecurityGroupIds: [secgrp.id],
+}, { replaceOnChanges: ["instanceType"] });
+
+const proxy = new aws.ec2.Instance("proxy", {
+    instanceType: size,
+    ami: ami.id,
+    keyName: keyName,
+    vpcSecurityGroupIds: [secgrp.id],
+}, { replaceOnChanges: ["instanceType"] });
+
+const proxyConnection: types.input.remote.ProxyConnectionArgs = {
+    host: proxy.publicIp,
+    user: "ec2-user",
+    privateKey: privateKey,
+}
+
+const connection: types.input.remote.ConnectionArgs = {
+    host: server.publicIp,
+    user: "ec2-user",
+    privateKey: privateKey,
+    proxy: proxyConnection,
+};
+
+const hostname = new remote.Command("hostname", {
+    connection: connection,
+    create: "hostname",
+    environment: secret({
+      "secret-key": secret("super-secret-value")
+    }),
+});
+
+new remote.Command("remotePrivateIP", {
+    connection,
+    create: interpolate`echo ${server.privateIp} > private_ip.txt`,
+    delete: `rm private_ip.txt`,
+}, { deleteBeforeReplace: true });
+
+new local.Command("localPrivateIP", {
+    create: interpolate`echo ${server.privateIp} > private_ip.txt`,
+    delete: `rm private_ip.txt`,
+}, { deleteBeforeReplace: true });
+
+const sizeFile = new remote.CopyFile("size", {
+    connection: connection,
+    localPath: "./size.ts",
+    remotePath: "size.ts",
+})
+
+const catSize = new remote.Command("checkSize", {
+    connection: connection,
+    create: "cat size.ts",
+}, { dependsOn: sizeFile })
+
+export const connectionSecret = hostname.connection;
+export const secretEnv = hostname.environment;
+export const confirmSize = catSize.stdout;
+export const publicIp = server.publicIp;
+export const publicHostName = server.publicDns;
+export const hostnameStdout = hostname.stdout;
