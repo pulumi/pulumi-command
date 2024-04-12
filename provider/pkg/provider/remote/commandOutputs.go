629c8312f8ab99abe4921230ca666d6919d6e514
Thomas Kappler <tkappler@pulumi.com>
2024-04-12 20:38:30 +0200

    Handle all errors

--- provider/pkg/provider/remote/commandOutputs.go
+++ provider/pkg/provider/remote/commandOutputs.go
@@ -42,18 +42,21 @@ func (c *CommandOutputs) run(ctx p.Context, cmd string) error {
 		for k, v := range c.Environment {
 			err := session.Setenv(k, v)
 			if err != nil {
-				ctx.Logf(diag.Error, `Unable to set '%s'. This only works if your SSH server is configured to accept
- these variables via AcceptEnv. Alternatively, if a Bash-like shell runs the command on the remote host, you could
- prefix the command itself with the variables in the form 'VAR=value command'`, k)
-				return fmt.Errorf("could not set environment variable '%s': %w", k, err)
+				return setenvErr(k, ctx, err)
 			}
 		}
 	}
 	if c.Stdout != "" {
-		session.Setenv(util.PULUMI_COMMAND_STDOUT, c.Stdout)
+		err := session.Setenv(util.PULUMI_COMMAND_STDOUT, c.Stdout)
+		if err != nil {
+			return setenvErr(util.PULUMI_COMMAND_STDOUT, ctx, err)
+		}
 	}
 	if c.Stderr != "" {
-		session.Setenv(util.PULUMI_COMMAND_STDERR, c.Stderr)
+		err := session.Setenv(util.PULUMI_COMMAND_STDERR, c.Stderr)
+		if err != nil {
+			return setenvErr(util.PULUMI_COMMAND_STDERR, ctx, err)
+		}
 	}
 
 	if c.Stdin != nil && len(*c.Stdin) > 0 {
@@ -81,4 +84,11 @@ func (c *CommandOutputs) run(ctx p.Context, cmd string) error {
 		Stderr: strings.TrimSuffix(stderrbuf.String(), "\n"),
 	}
 	return nil
+}
+
+func setenvErr(key string, ctx p.Context, err error) error {
+	ctx.Logf(diag.Error, `Unable to set '%s'. This only works if your SSH server is configured to accept
+ these variables via AcceptEnv. Alternatively, if a Bash-like shell runs the command on the remote host, you could
+ prefix the command itself with the variables in the form 'VAR=value command'`, key)
+	return fmt.Errorf("could not set environment variable %q: %w", key, err)
 }
