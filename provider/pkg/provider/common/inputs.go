01a81799dca78b7a6f08639409e7c43fefba7ae2
Thomas Kappler <tkappler@pulumi.com>
2024-06-15 16:00:58 +0200

    Document Command triggers better, with examples

--- provider/pkg/provider/common/inputs.go
+++ provider/pkg/provider/common/inputs.go
@@ -17,7 +17,38 @@ type ResourceInputs struct {
 // Annotate lets you provide descriptions and default values for fields and they will
 // be visible in the provider's schema and the generated SDKs.
 func (c *ResourceInputs) Annotate(a infer.Annotator) {
-	a.Describe(&c.Triggers, "Trigger replacements on changes to this input.")
+	a.Describe(&c.Triggers, `Trigger a resource replacement on changes to any of these values. The
+trigger values can be of any type. If a value is different in the current update compared to the
+previous update, the resource will be replaced, i.e., the "create" command will be re-run.
+
+Here are some examples of triggers in TypeScript. Changes to any of the four will cause `+"`cmd`"+
+		` to be re-run. However, note that for `+"`fileAsset`"+` it's the variable itself that is
+the trigger, not the contents of index.ts, since triggers are simply opaque values.
+
+`+"```typescript"+
+		`import * as local from "@pulumi/command/local";
+import * as random from "@pulumi/random";
+import { asset } from "@pulumi/pulumi";
+import * as path from "path";
+
+const str = "foo";
+
+const rand = new random.RandomString("rand", { length: 5 });
+
+const fileAsset = new asset.FileAsset(path.join(__dirname, "index.ts"));
+
+const localFile = new local.Command("localFile", {
+    create: `+"`touch foo.txt`"+`,
+    archivePaths: ["*.txt"]
+});
+
+const cmd = new local.Command("pwd", {
+    create: `+"`echo create > op.txt`"+`,
+    delete: `+"`echo delete >> op.txt`"+`,
+    triggers: [str, rand.result, fileAsset, localFile.archive],
+});`+
+		"```")
+
 	a.Describe(&c.Create, "The command to run on create.")
 	a.Describe(&c.Delete, `The command to run on delete. The environment variables PULUMI_COMMAND_STDOUT
 and PULUMI_COMMAND_STDERR are set to the stdout and stderr properties of the
